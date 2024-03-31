1aa3f972f1056e136c8557c0ccd7a2b3c7cfe9b6
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-03-31 22:17:11 +0430

    [ Add Model UserLimit and  EmailAccount ]
     The User Limit is for limiting the creation of user emails and consists of three fields. The first field is "user," which is for obtaining the user. The second field is "email_count," which determines the number of user emails to be created. If a user purchases a subscription, this value may increase or decrease. The third field is "creation_date," which is for obtaining the date of creation.
     --------------------
     The EmailAccount is used for storing created emails and consists of four fields. The first field is "email_username" for the email username. The second field is "email_password" for the email password. The third field is for the allocated storage space for the email. And the fourth field is "user" for obtaining the user account.

--- email_land_project/email_generator/models.py
+++ email_land_project/email_generator/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class UserLimit(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_count = models.IntegerField(default=2)
+    creation_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.username
+
+
+class EmailAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email_username = models.EmailField()
+    email_password = models.CharField(max_length=400)
+    email_quota = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email_username
